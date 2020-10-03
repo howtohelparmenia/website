503629394c130bc7f8b63f78b0d2b4c5509d5856
Aram Boyajyan <dev@aram.cz>
2020-10-03 12:36:10 +0200

    renaming public_html to docs; adding onPostBuild action to rename it automatically

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,12 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+// Rename "/public" to "/docs" once the build is done. This is necessary for
+// Github pages to work. Github doesn't give an option to change this to a
+// different directory and Gatsby hardcodes the "/public" path.
+// https://github.com/gatsbyjs/gatsby/issues/14703
+const path = require('path');
+const fs = require('fs');
+exports.onPostBuild = function() {
+  fs.renameSync(path.join(__dirname, 'public'), path.join(__dirname, 'docs'));
+};
